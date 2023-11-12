219173c726da5ca7849aabf2275907b5054ce79f
gb0ye <danieladeribigbe74@gmail.com>
2023-11-12 23:51:14 +0100

    fix: correct port error

--- server/server.js
+++ server/server.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const path = require("path");
-const { createServer } = require('node:http');
+const { createServer } = require("node:http");
 const socketServer = require("./websocket/socket");
 
 const app = express();
 const server = createServer(app);
 socketServer.attach(server);
 
-app.use(express.static(path.join(__dirname, '../static')));
+app.use(express.static(path.join(__dirname, "../static")));
 
 app.get("/home", (req, res) => {
    const homePath = path.join(__dirname, "../static/index.html");
    res.sendFile(homePath);
 });
 
-server.listen(3000, () => {
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
    console.log("server is running on port localhost:3000");
 });
